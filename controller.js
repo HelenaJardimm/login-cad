859ee90b2fd642caf9a65537b61debcb8d0bba9d
HelenaJardimm <234jardim@gmail.com>
2024-07-04 08:30:17 -0300

    FUNÇÃO DE CRIAÇÃO DE ARRAY PARA ARMAZENAMENTO DE NOMES

--- controller.js
+++ controller.js
@@ -7,6 +7,20 @@ function acessar(){
         alert('Favor preencher todos os campos');
     } else{
        // alert("Campos preenchidos com sucesso");
-       window.location.href = 'cadstro.html';
+       window.location.href = 'cadastro.html';
+    }
+}
+
+//FUNÇÃO DE CRIAÇÃO DE ARRAY PARA ARMAZENAMENTO DE NOMES 
+
+var dadosLista = [];
+
+function salvarUser(){
+    let nomeUser = document.getElementById('nomeUser').value;
+
+    if(nomeUser){
+
+    } else{
+        alert("Favor, informar um nome");
     }
 }
