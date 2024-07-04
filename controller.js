a72bfc7082c6784ca4b0a47e12f56bb1fae36132
HelenaJardimm <234jardim@gmail.com>
2024-07-04 07:58:51 -0300

    direcionando para a pagina de cadastro

--- controller.js
+++ controller.js
@@ -2,5 +2,11 @@
 function acessar(){
     let loginEmail = document.getElementById('loginEmail').value;
     let loginSenha = document.getElementById('loginSenha').value;
-    
+
+    if(!loginEmail || !loginSenha){
+        alert('Favor preencher todos os campos');
+    } else{
+       // alert("Campos preenchidos com sucesso");
+       window.location.href = 'cadstro.html';
+    }
 }
