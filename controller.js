30c32318296f570c89e4d3f8611f1abffd2f8a74
HelenaJardimm <234jardim@gmail.com>
2024-08-08 08:51:41 -0300

    função para editar o nome da lista

--- controller.js
+++ controller.js
@@ -1,17 +1,18 @@
-//VALIDAÇÃO DE PREENCHIMENTOet
+// Validação de preenchimento
 function acessar(){
     let loginEmail = document.getElementById('loginEmail').value;
     let loginSenha = document.getElementById('loginSenha').value;
  
     if(!loginEmail || !loginSenha){
         alert('Favor preencher todos os campos');
-    } else{
-       // alert("Campos preenchidos com sucesso");
-       window.location.href = 'cadastro.html';
+    }else{
+        alert('Campos preenchidos com sucesso');
+       
+        window.location.href = 'cadastro.html';
     }
 }
  
-//FUNÇÃO DE CRIAÇÃO DE ARRAY PARA ARMAZENAMENTO DE NOMES
+//Função de criação do array para armazenamento de dados
  
 var dadosLista = [];
  
@@ -19,30 +20,35 @@ function salvarUser(){
     let nomeUser = document.getElementById('nomeUser').value;
  
     if(nomeUser){
-      dadosLista.push(nomeUser);
-      console.log(dadosLista);
-      crialista();
-      document.getElementById('nomeUser').value = "";
- 
-    } else{
-        alert("Favor, informar um nome para cadastro");
+        dadosLista.push(nomeUser);
+        console.log(dadosLista);
+        criarLista();
+        document.getElementById('nomeUser').value = "";
+    }
+    else{
+        alert("Favor informar o nome");
+    }
+}
+// FUNÇÃO DE CRIAÇÃO DE LISTA
+function criarLista(){
+    let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
+    for(let i = 0; i <= (dadosLista.length -1); i++){ // A propriedade length tem como responsabilidade retornar a quantidade de caracteres de uma string ou o tamanho de um array. Caso a string ou o array esteja vazio, é retornado o valor 0.
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td>  <button onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button> <button onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button> </td></tr>";
+        document.getElementById('tabela').innerHTML = tabela;
     }
 }
  
-  //FUNÇÃO DE CRIAÇÃO DE LISTA
-  function crialista(){
-   let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th>";
-  //LAÇO DE REPETIÇÃO
-   for(let i = 0; i <=(dadosLista.length - 1); i ++){
-     tabela += "<tr><td>" + dadosLista[i] + "</td><td><button onclick=''> Excluir </button></td></tr>";
-     document.getElementById('tabela').innerHTML = tabela;
-   }
-  }
-
-  //FUNÇÃO  PARA EXCLUIR NOME DE LISTA
-
-  function excluir(i){
-     dadosLista.splice((i - 1), 1);
-     document.getElementById('tabela').deleteRow(i);
-  }
+// FUNÇÃO PARA EXCLUIR NOME DA LISTA
+ 
+function excluir(i){
+    dadosLista.splice((i - i), 1); //Splice é uma das maneiras mais úteis de manipular arrays em JavaScript. Ele permite remover ou adicionar elementos em uma posição específica de um array
+    document.getElementById('tabela').deleteRow(i);
+}
+ 
+//FUNÇÃO PARA EDITAR O NOME DA LISTA
+ 
+function editar(i){
+    document.getElementById("nomeUser").value = dadosLista[(i - 1)];
+    dadosLista.splice(dadosLista[(i - 1)], 1);
+}
  
